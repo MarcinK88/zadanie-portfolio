c1739ce48ff91b22f787fba420093b78efb27cc9
MarcinK88 <marcin3216@gmail.com>
2020-07-01 19:05:12 +0200

    OurProjects

--- src/components/OurProjects/index.js
+++ src/components/OurProjects/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import s from './style.module.css'
+import Project from './components/Project'
 
-export default function OurProjects() {
+export default function OurProjects({ title, service }) {
     return (
-        <div>
-            OurProjects
+        <div className="container-fluid">
+            <div className={s.root} >
+                <h3 className="mb-5">{title}</h3>
+
+                <div className="row">
+
+                    {service.map((project) => (
+                        <div className="col-4 position-relative">
+                            <Project {...project} />
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
