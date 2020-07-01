5094f21184eec673d38b79d9bd1994ec56889db9
MarcinK88 <marcin3216@gmail.com>
2020-07-01 19:46:27 +0200

    bloglist

--- src/components/BlogList/index.js
+++ src/components/BlogList/index.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import s from './style.module.css'
+import Post from './components/Post'
 
-export default function BlogList() {
+export default function BlogList({ title, posts }) {
+    const [slicedPosts, setSlicedPosts] = useState([]);
+    const [hideButton, setHideButton] = useState(false);
+
+    useEffect(() => {
+        setSlicedPosts(
+            posts.slice(0, 3),
+        )
+    }, [posts]);
+
+    function showMore() {
+        setSlicedPosts(
+            posts
+        );
+        setHideButton(true);
+    }
+
     return (
-        <div>
-            BlogList
+        <div className="container">
+            <h2 className="text-center">{title}</h2>
+            <div className="row">
+                {slicedPosts.map((post) => (
+                    <div className="col-4">
+                        <Post {...post} />
+                    </div>
+                ))}
+            </div>
+            {!hideButton &&
+                <button className="btn btn-primary" onClick={showMore}>Read More!!</button>
+            }
         </div>
     )
 }
