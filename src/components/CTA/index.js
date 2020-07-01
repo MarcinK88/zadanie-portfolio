517ce5dffff67db696d7bda5230c3fa0628584e1
MarcinK88 <marcin3216@gmail.com>
2020-07-01 18:17:56 +0200

    CTA

--- src/components/CTA/index.js
+++ src/components/CTA/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import s from './style.module.css'
 
-export default function CTA() {
+export default function CTA({ title, subtitle, background, opacity, button }) {
     return (
-        <div>
-            CTA
-        </div>
+        <div className={s.root}>
+            <div className={s.bg} style={{ backgroundImage: `url(${background})`, opacity: opacity }} / >
+                <div className="d-flex align-items-center h-100">
+                    <div className={s.text}>
+                        <h3>{title}</h3>
+                        <div className="mt-4 mb-4">{subtitle}</div>
+                        <button className="btn btn-secondary">{button}</button>
+                    </div>
+              
+            </div >
+        </div >
     )
 }
