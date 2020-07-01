aed534e40e3706a4c13dea61ed22c3f0abae5746
MarcinK88 <marcin3216@gmail.com>
2020-07-01 20:03:01 +0200

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import s from './style.module.css'
 
-export default function Footer() {
+export default function Footer({ title, subtitle, phoneIcon, envelopeIcon, phone, email }) {
     return (
-        <div>
-            Footer
+        <>
+        <div className="container">
+            <div className={s.root}>
+                <h3>{title}</h3>
+                <div className="mb-5">{subtitle}</div>
+                <div className={s.contactDetails}>
+                    <img src={phoneIcon} />{phone}
+                </div>
+                <div className={s.contactDetails}>
+                    <img src={envelopeIcon} />{email}
+                </div>
+            </div>
         </div>
+
+        </>
     )
-}
+} 
